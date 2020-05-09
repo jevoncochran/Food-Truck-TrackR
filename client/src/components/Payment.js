e9ca93ba871062cb37d24d5d006a4aeafcea05dc
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-09 00:41:22 -0300

    more work on Payment component

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import Header from "./Header";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import CurrencyFormatter from "currencyformatter.js";
 import "../styling/Payment.scss"
 
 const Payment = props => {
+    const orderCount = props.order.reduce(function(prev, cur) {
+        return prev + cur.count;
+    }, 0);
+
     return (
         <div className="payment-main">
             <Header history={props.history} />
@@ -10,24 +20,49 @@ const Payment = props => {
             <div className="payment-body">
                 <div className="order-div">
                     <div>
-                        <h2>Payment</h2>
-                        <p>Master -5559</p>
-                        <p>Pay in cash</p>
+                        <div className="payment-title-div">
+                            <h2>Payment</h2>
+                            <button className="edit-payment-btn">Edit</button>
+                        </div>
+                        <hr />
+                        <FormControl component="fieldset">
+                            <RadioGroup aria-label="gender" name="gender1" value={null} onChange={null}>
+                                <FormControlLabel value="female" control={<Radio />} label="Mastercard -5559" className="payment-radio"/>
+                                <FormControlLabel value="male" control={<Radio />} label="Pay in cash" className="payment-radio"/>
+                            </RadioGroup>
+                        </FormControl>
+                        <hr />
                     </div>
 
                     <div>
-                        <h2>Your Order</h2>
-                        <p>From truck</p>
-                        <p>item 1</p>
-                        <p>item 2</p>
-                        <input type="text" placeholder="Note to truck..." />
+                        <div className="order-title-div">
+                            <h2>Your Order</h2>
+                            <button className="add-items-order-btn">Add items</button>
+                        </div>
+                        <p className="order-div-truck">From {props.selectedTruck.name}</p>
+
+                        <div className="order-items-cont">
+                            {props.order.map(item => (
+                                <div key={item.item} className="order-item-div">
+                                    <i class="fas fa-times-circle" style={{ width: '10%', paddingTop: 'auto', paddingBottom: 'auto' }} onClick={() => props.removeFromOrder(item.item)}></i>
+                                    <p className="order-item-count">
+                                        <input type="number" value={item.count} onChange={ e => props.updateCount(Number(e.target.value), item.item)} className="order-item-count-input" />
+                                    </p>
+                                    <p className="order-item-name">{item.item}</p>
+                                    <p className="order-item-total">{CurrencyFormatter.format(item.total, { currency: 'USD' })}</p>
+                                    <hr />
+                                </div>
+                            ))}
+                        </div>
+                        <input type="text" placeholder="Note to truck..." className="note-input" />
                     </div>
                 </div>
 
-                <div className="confirm-payment-div">
-                    <h2>From truck</h2>
+                <div className="confirm-pay-div">
+                    <h2 className="confirm-pay-div-truck">From {props.selectedTruck.name}</h2>
+                    <hr />
                     <div style={{ display: 'flex' }}>
-                        <p>Subtotal - 1 item</p>
+                        <p>Subtotal - {orderCount} {orderCount > 1 ? "items" : "item"}</p>
                         <p>$20.50</p>
                     </div>
                     <p>Add tip</p>
@@ -35,11 +70,18 @@ const Payment = props => {
                         <p>Total</p>
                         <p>$20.50</p>
                     </div>
-                    <button>Confirm Order</button>
+                    <button className="confirm-order-btn">Confirm Order</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Payment;
+const mapStateToProps = state => {
+    return {
+        selectedTruck: state.selectedTruck,
+        order: state.order
+    }
+}
+
+export default connect(mapStateToProps, {})(Payment);
