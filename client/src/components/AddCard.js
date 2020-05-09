d6189d04d18b6493e3daf10d7ead8e865b813c55
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-09 17:40:16 -0300

    AddCard component 80% done

--- client/src/components/AddCard.js
+++ client/src/components/AddCard.js
@@ -3,6 +3,7 @@ import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
+import '../styling/AddCard.scss';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -10,8 +11,8 @@ const useStyles = makeStyles((theme) => ({
     //   top: '50%',
     //   left: '50%',
       width: 400,
-      height: '85vh',
-      minHeight: '30vh',
+      minHeight: '85vh',
+      height: '90vh',
       backgroundColor: theme.palette.background.paper,
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
@@ -45,61 +46,84 @@ const AddCard = props => {
         aria-describedby="simple-modal-description"
         >
             <div style={modalStyle} className={classes.paper}>
-                <h2>Add credit or debit card</h2>
-                <Cards 
-                    number={number}
-                    name={name}
-                    expiry={expiry}
-                    cvc={cvc}
-                    focused={focus}
-                />
-                <form>
-                    <label htmlFor="number">Card Number</label><br />
-                    <input 
-                    type='tel' 
-                    name='number' 
-                    value={number} 
-                    onChange={e => setNumber(e.target.value)}
-                    onFocus={e => setFocus(e.target.name)} 
-                    /><br />
-
-                    <label htmlFor="name">Name</label><br />
-                    <input 
-                    type='text' 
-                    name='name' 
-                    value={name} 
-                    onChange={e => setName(e.target.value)}
-                    onFocus={e => setFocus(e.target.name)} 
-                    /><br />
-
-                    <label htmlFor="expiry">Exp. Date</label><br />
-                    <input 
-                    type='text' 
-                    name='expiry' 
-                    placeholder='MM/YY' 
-                    value={expiry} 
-                    onChange={e => setExpiry(e.target.value)}
-                    onFocus={e => setFocus(e.target.name)} 
+                
+                <div className="add-card-body">
+                    <i class="fas fa-times" onClick={props.closeCardModal} style={{ marginTop: '2%', fontSize: '1.2rem', marginBottom: '4%' }}></i>
+                    <Cards 
+                        number={number}
+                        name={name}
+                        expiry={expiry}
+                        cvc={cvc}
+                        focused={focus}
                     />
-
-                    <label htmlFor="cvc">Security Code</label><br />
-                    <input 
-                    type='tel' 
-                    name='cvc' 
-                    value={cvc} 
-                    onChange={e => setCvc(e.target.value)}
-                    onFocus={e => setFocus(e.target.name)} 
-                    />
-
-                    <label htmlFor="zip">Zip Code</label><br />
-                    <input 
+                    <form>
+                        <label htmlFor="number" style={{ marginBottom: '2%' }}>Card Number</label>
+                        <input 
                         type='tel' 
-                        name='zip' 
-                        value={zip} 
-                        onChange={e => setZip(e.target.value)}
+                        name='number' 
+                        value={number} 
+                        onChange={e => setNumber(e.target.value)}
                         onFocus={e => setFocus(e.target.name)} 
-                    />
-                </form>
+                        className="card-num-input"
+                        />
+
+                        <label htmlFor="name" style={{ marginBottom: '2%' }}>Name</label>
+                        <input 
+                        type='text' 
+                        name='name' 
+                        value={name} 
+                        onChange={e => setName(e.target.value)}
+                        onFocus={e => setFocus(e.target.name)} 
+                        className="card-name-input"
+                        />
+
+                        <div className="exp-sec-div">
+                        <div className="exp-sec-label-div">
+                            <div className="exp-label-div" style={{ marginBottom: '2%' }}>
+                                <label htmlFor="expiry">Exp. Date</label>
+                            </div>
+                            <div className="sec-label-div">
+                                <label htmlFor="cvc">Security Code</label>
+                            </div>
+                        </div>
+                        <div className="exp-sec-input-div">
+                            <div className="exp-input-div">
+                                <input 
+                                type='text' 
+                                name='expiry' 
+                                placeholder='MM/YY' 
+                                value={expiry} 
+                                onChange={e => setExpiry(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)} 
+                                />
+                            </div>
+
+                            <div className="sec-input-div">
+                                <input 
+                                type='tel' 
+                                name='cvc' 
+                                value={cvc} 
+                                onChange={e => setCvc(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)} 
+                                />
+                            </div>
+                        </div>
+                        </div>
+
+                        <div className="zip-div">
+                            <label htmlFor="zip" style={{ marginBottom: '2%' }}>Zip Code</label>
+                            <input 
+                                type='tel' 
+                                name='zip' 
+                                value={zip} 
+                                onChange={e => setZip(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)} 
+                            />
+                        </div>
+
+                        <button type="submit" className="add-card-btn">Add Card</button>
+                    </form>
+                </div>
             </div>
         </Modal>
     )
