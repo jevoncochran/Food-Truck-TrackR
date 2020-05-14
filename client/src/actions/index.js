1632fd409e91d44dc4b65da00d31bf2987add108
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-14 18:28:16 -0300

    modified actions file

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -89,7 +89,7 @@ export const registerDiner = (info) => (dispatch) => {
   dispatch({ type: SIGNUP_START });
   axios
     .post(
-      "http://localhost:5000/api/auth/register/diners",
+      "https://foodtrucktrackr.herokuapp.com/api/auth/register/diners",
       info
     )
     .then((res) => {
